24f0ec37a9ae9ee4c1b9e74fa0066ee95cb21300
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-01-27 00:45:31 +0500

    Updated

--- Python-Problems/15_averageHeightStudent.py
+++ Python-Problems/15_averageHeightStudent.py
@@ -1,13 +1,23 @@
-students = []
-for student in range( 0, 5):
-    height = int(input("Enter height of students: "))
-    students.append(height)
+# students = []
+# for student in range( 0, 5):
+#     height = int(input("Enter height of students: "))
+#     students.append(height)
 
-print(students)
-sumOfheight = 0
-for height in students:
-    sumOfheight+= height
+# print(students)
+# sumOfheight = 0
+# for height in students:
+#     sumOfheight+= height
     
-avgHeight = sumOfheight/len(students)
-print(avgHeight)
+# avgHeight = sumOfheight/len(students)
+# print(avgHeight)
     
+    
+student_heights = input("Enter a list of student heights ").split()
+for n in  range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+print(student_heights)
+sumOfHeight = 0
+for height in student_heights:
+    sumOfHeight += height
+avHeight = sumOfHeight/len(student_heights)
+print(avHeight)
