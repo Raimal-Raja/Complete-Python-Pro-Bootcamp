85cee2235656160da53b5ae14bd2855928959675
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-01-28 21:43:40 +0500

    Updated

---
+++ Python-Problems/18_fizzBuzzJobInterview.py
@@ -0,0 +1,12 @@
+fizz = 3
+buzz = 5
+
+for i in range(1, 100):
+    if i % fizz == 0 and i % buzz == 0:
+        print('FizzBuzz')
+    elif i % fizz == 0:
+        print('Fizz')
+    elif i % buzz == 0:
+        print('Buzz')
+    else:
+        print(i)
