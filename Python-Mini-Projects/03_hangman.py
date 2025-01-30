028acda2f710c2b1741e69c3ab48bc04cd7ef57e
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-01-30 14:44:41 +0500

    Updated

--- Python-Mini-Projects/03_hangman.py
+++ Python-Mini-Projects/03_hangman.py
@@ -1,17 +1,32 @@
 import random
 words = ["madhu", "tara", "sitara"]
 
-randomChoose = random.choice(words)
+choosenWord = random.choice(words)
 
 display = []
-for char in randomChoose:
+for char in choosenWord:
     display += "_"
 
-guessLetter = input('Guess the letter').lower()
-for index,letter in enumerate(randomChoose):
-    if letter == guessLetter:
-        print('Right')
-        display[index] =  letter 
-    else:
-        print("wrong")
+counter = len(choosenWord)
+# winner = 0
+while counter > 0:
+    guessLetter = input('Guess the letter: ').lower()
+    for index,letter in enumerate(choosenWord):
+        if letter == guessLetter:
+            print('Right')
+            # winner += 1
+            display[index] =  letter 
+        else:
+            print("wrong")
+    counter -= 1
+    print(display)
+# if winner == len(choosenWord):
+if not '_' in display:
+    print("You Win")
+#     print("You Win")
+# else:
+#     print('You lose!')
+else:
+    print('You lose!')
+display = ''.join(display)
 print(display)
