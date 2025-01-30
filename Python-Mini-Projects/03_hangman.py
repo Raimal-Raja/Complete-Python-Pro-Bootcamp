36d8f99718a506513708204c4ec16b35b95e2aec
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-01-30 23:21:06 +0500

    Updated

--- Python-Mini-Projects/03_hangman.py
+++ Python-Mini-Projects/03_hangman.py
@@ -1,8 +1,77 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+print(logo)
 words = ["madhu", "tara", "sitara"]
 
 choosenWord = random.choice(words)
-
+lives = 6
 display = []
 for char in choosenWord:
     display += "_"
@@ -11,14 +80,20 @@ counter = len(choosenWord)
 # winner = 0
 while counter > 0:
     guessLetter = input('Guess the letter: ').lower()
-    for index,letter in enumerate(choosenWord):
-        if letter == guessLetter:
-            print('Right')
-            # winner += 1
-            display[index] =  letter 
-        else:
-            print("wrong")
+    # for index,letter in enumerate(choosenWord):
+    #     if letter == guessLetter:
+    #         print('Right')
+    #         # winner += 1
+    #         display[index] =  letter 
+    #     else:
+    #         print("wrong")
+    for position in range(len(choosenWord)):
+        letter = choosenWord[position]
+        if letter ==  guessLetter:
+            display[position] = letter
+            lives -= 1
     counter -= 1
+    print(stages[lives])
     print(display)
 # if winner == len(choosenWord):
 if not '_' in display:
