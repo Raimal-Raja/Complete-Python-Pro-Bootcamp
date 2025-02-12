91e167cc54c3d503628097f36eb86a4e77db050d
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-12 21:45:26 +0500

    Updated

--- Python-Problems/20_areaCal.py
+++ Python-Problems/20_areaCal.py
@@ -1,7 +1,19 @@
-height = int(input("Enter height in meter: "))
-width = int(input("Enter weidth in meter: "))
+# height = int(input("Enter height in meter: "))
+# width = int(input("Enter weidth in meter: "))
 
-print('Calculating...')
-numberOfCan = height * width / 5
-round(numberOfCan, 2)
-print(f'You will need {numberOfCan} cans of paint.')
+# print('Calculating...')
+# numberOfCan = height * width / 5
+# round(numberOfCan, 2)
+# print(f'You will need {numberOfCan} cans of paint.')
+
+
+# same thing using functions
+def paint_calc(height, width, cover):
+    numberOfcan = height * width / cover
+    round(numberOfcan, 2)
+    print(f'You will need to buy {numberOfcan} cans of paint.')
+
+test_h = int(input("Enter height of Wall in meter: "))
+test_w = int(input("Enter width of wall in meter: "))
+coverage = 5
+paint_calc(height = test_h, width = test_w, cover = coverage)
