068ed9b20186f32dcfdd09b3a71738406c359629
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-14 23:04:02 +0500

    Updated

--- Python-Problems/23_caeserCipher_2.py
+++ Python-Problems/23_caeserCipher_2.py
@@ -3,16 +3,15 @@ alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 Direction = input("Type 'encode' to encrypt and 'decode' to decrypt: ")
 text = input("type your message: ").lower()
 shift = int(input("Type the shift number: "))
-
-
-def encrypt(plain_text, shift_amount):
+   
+    
+def decrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
         position = alphabet_list.index(letter)
-        new_position = position + shift_amount
+        new_position = position - shift_amount
         new_letter = alphabet_list[new_position]
         cipher_text += new_letter
-    print(f'The encoded text is {cipher_text}')
-   
-
-encrypt(plain_text= text,shift_amount= shift)
+    print(f'The decrypt text is {cipher_text}')
+    
+decrypt(plain_text=text, shift_amount= shift)
