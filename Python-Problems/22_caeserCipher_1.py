1b13aa38a7a6861db1a1f6eb9a9e00fc29b925a1
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-14 22:44:20 +0500

    Updated

--- Python-Problems/22_caeserCipher_1.py
+++ Python-Problems/22_caeserCipher_1.py
@@ -1,4 +1,17 @@
-alphabet_list = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+# alphabet_list = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
 Direction = input("Type 'encode' to encrypt and 'decode' to decrypt: ")
 text = input("type your message: ").lower()
 shift = int(input("Type the shift number: "))
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet_list.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet_list[new_position]
+        cipher_text += new_letter
+    print(f'The encoded text is {cipher_text}')
+    
+
+encrypt(plain_text= text, shift_amount= shift)
