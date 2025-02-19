428496dfa4937466eacf2d177e41676658c4624a
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-20 00:37:24 +0500

    Updated

---
+++ Python-Problems/33_4_calculatorDict.py
@@ -0,0 +1,53 @@
+# calculator
+
+# add
+def add (n1, n2):
+    return n1 + n2
+
+# substract
+def sub(n1, n2):
+    return n1 - n2
+
+# multiplication
+def multi(n1, n2):
+    return n1 * n2
+
+# division
+def div(n1, n2):
+    if n2 == 0:
+        return "Error! Division by zero."
+    return n1 / n2
+
+n1 = int(input('Enter number1: '))
+
+
+
+operations = {
+    '+':add,
+    '-':sub,
+    '*':multi,
+    '/':div
+}
+
+should_continue = True
+while should_continue:
+    print("Available operations: " + " ".join(operations.keys()))
+    operation_symbol = input('Pick an operation from the list above: ')
+    if operation_symbol not in operations:
+        print("Invalid operation. Please try again.")
+        continue
+    n2 = int(input('Enter number2: '))
+    calculate_function = operations[operation_symbol]
+    result = calculate_function(n1, n2)
+    print(f'{n1} {operation_symbol} {n2} = {result}')
+    # if input(f'Type "Y" to continue calculing with {result}, or type "n" to exit: ') =="y":
+    conti = input(f'Type "Y" to continue calculing with {result}, or type "n" to exit: ')
+    if conti =='y':
+        n1 == result
+    elif conti == 'n':
+        should_continue = False
+    else:
+        print("Invalid input. Exiting calculator.")
+        should_continue = False
+
+print("Thank you for using the calculator!")
