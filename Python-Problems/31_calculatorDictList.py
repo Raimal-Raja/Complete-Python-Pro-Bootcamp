b258d2f18734a0dfe62fd29806834b5789d3f95c
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-19 15:26:54 +0500

    Updated

---
+++ Python-Problems/31_calculatorDictList.py
@@ -0,0 +1,43 @@
+# calculator
+
+# add
+def add (n1, n2):
+    return n1 + n2
+
+# substract
+def sub(n1, n2):
+    return n1 - n2
+
+# multiplication
+def multi(n1, n2):
+    return n1 * n2
+
+# division
+def div(n1, n2):
+    if n2 == 0:
+        return "Error! Division by zero."
+    return n1 / n2
+
+n1 = int(input('Enter number1: '))
+n2 = int(input('Enter number2: '))
+
+
+
+operations = {
+    '+':add,
+    '-':sub,
+    '*':multi,
+    '/':div
+}
+
+
+
+for operator in operations:
+    print(operator)
+operator = input('which operation have to perform?')
+
+if operator in operations:
+    result = operations[operator](n1, n2)  # Call the function with n1, n2
+    print("Result:", result)
+else:
+    print('Invalid input!')
