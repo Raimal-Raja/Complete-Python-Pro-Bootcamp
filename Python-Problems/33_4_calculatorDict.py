57d4c4bbb33667814dfd9149f439b30786f84041
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-20 00:50:06 +0500

    Updated

--- Python-Problems/33_4_calculatorDict.py
+++ Python-Problems/33_4_calculatorDict.py
@@ -18,8 +18,6 @@ def div(n1, n2):
         return "Error! Division by zero."
     return n1 / n2
 
-n1 = int(input('Enter number1: '))
-
 
 
 operations = {
@@ -29,25 +27,32 @@ operations = {
     '/':div
 }
 
-should_continue = True
-while should_continue:
-    print("Available operations: " + " ".join(operations.keys()))
-    operation_symbol = input('Pick an operation from the list above: ')
-    if operation_symbol not in operations:
-        print("Invalid operation. Please try again.")
-        continue
-    n2 = int(input('Enter number2: '))
-    calculate_function = operations[operation_symbol]
-    result = calculate_function(n1, n2)
-    print(f'{n1} {operation_symbol} {n2} = {result}')
-    # if input(f'Type "Y" to continue calculing with {result}, or type "n" to exit: ') =="y":
-    conti = input(f'Type "Y" to continue calculing with {result}, or type "n" to exit: ')
-    if conti =='y':
-        n1 == result
-    elif conti == 'n':
-        should_continue = False
-    else:
-        print("Invalid input. Exiting calculator.")
-        should_continue = False
+def calculator():
+    n1 = int(input('Enter number1: '))
+    should_continue = True
+    while should_continue:
+        print("Available operations: " + " ".join(operations.keys()))
+        operation_symbol = input('Pick an operation from the list above: ')
+        if operation_symbol not in operations:
+            print("Invalid operation. Please try again.")
+            continue
+        n2 = int(input('Enter number2: '))
+        calculate_function = operations[operation_symbol]
+        result = calculate_function(n1, n2)
+        print(f'{n1} {operation_symbol} {n2} = {result}')
+        # if input(f'Type "Y" to continue calculing with {result}, or type "n" to exit: ') =="y":
+        conti = input(f'Type "Y" to continue calculing with {result},or type "n" to exit, and "s" for starting from fresh:  ')
+        if conti =='y':
+            n1 == result
+        elif conti == 'n':
+            print("Thank you for using the calculator!")
+            return 
+        elif conti =='s':
+            print("Restarting calculator...\n")
+            calculator()
+        else:
+            print("Invalid input. Exiting calculator.")
+            should_continue = False
 
+calculator()
 print("Thank you for using the calculator!")
