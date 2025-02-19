dd504724a14074016379ec60c1155262cc282c33
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-19 15:54:47 +0500

    Updated

--- Python-Problems/31_calculatorDict.py
+++ Python-Problems/31_calculatorDict.py
@@ -32,10 +32,18 @@ operations = {
 
 for operator in operations:
     print(operator)
-operand = input('which operation have to perform?')
+operation_symbol = input('Pick an operation from the line above: ')
+calculate_function = operations[operation_symbol]
+answer = calculate_function(n1, n2) 
 
-if operator in operations:
-    result = operations[operator](n1, n2)  # Call the function with n1, n2
-    print(f"{n1} {operand} {n2} = {result}")
-else:
-    print('Invalid input!')
+print(f"{n1} {operation_symbol} {n2} = {answer}")
+
+
+    
+# operand = input('which operation have to perform?')
+
+# if operator in operations:
+#     result = operations[operator](n1, n2)  # Call the function with n1, n2
+#     print(f"{n1} {operand} {n2} = {result}")
+# else:
+#     print('Invalid input!')
