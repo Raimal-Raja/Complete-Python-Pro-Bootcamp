4d453544c8c3291eb23a499696f89aac384127fc
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-21 14:37:12 +0500

    Update 08_blackjackCard.py

--- Python-Mini-Projects/08_blackjackCard.py
+++ Python-Mini-Projects/08_blackjackCard.py
@@ -2,6 +2,10 @@ print("Welcome to the Blackjack Program")
 import random
 
 def deal_card ():
+    '''Return a random card from the deck.'''
     cards = [11,2,3,4,5,6,7,8,9,10,10,10]
     card = random.choice(cards)
     return card
+
+user_card = []
+computer_card = []
