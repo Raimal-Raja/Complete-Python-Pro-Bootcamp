6eff232c031d332bf7a0389c529ea8c54497ed7d
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-21 23:15:24 +0500

    Updated

--- Python-Mini-Projects/08_blackjackCard.py
+++ Python-Mini-Projects/08_blackjackCard.py
@@ -9,7 +9,7 @@ def deal_card ():
 
 def calculate_score(cards):
     """take a list of cards and return the score calculate from from the card"""
-    if sum(cards) ==21 and len(cards) == 2:
+    if sum(cards) == 21 and len(cards) == 2:
         return 0
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
@@ -23,11 +23,18 @@ for _ in range(2):
     user_card.append(deal_card())
     computer_card.append(deal_card())
     
-user_score = calculate_score(user_card)
-computer_score = calculate_score(computer_card)
+while not is_game_over:
+    user_score = calculate_score(user_card)
+    computer_score = calculate_score(computer_card)
 
-print(f"Your card: {user_card}, current score: {user_score}")
-print(f"Computer first card: {computer_card[0]}")
+    print(f"Your card: {user_card}, current score: {user_score}")
+    print(f"Computer first card: {computer_card[0]}")
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input('Type "Y" to get another card, or "N" to pass: ')
+        if user_should_deal == "y":
+            user_card.append(deal_card())
+        else:
+            is_game_over = True
