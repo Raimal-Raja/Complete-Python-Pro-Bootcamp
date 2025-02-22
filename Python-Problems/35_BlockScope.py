81cfcf561c28aa5ab93c53f836253d832341389c
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-22 10:41:49 +0500

    Updated

---
+++ Python-Problems/35_BlockScope.py
@@ -0,0 +1,7 @@
+game_level = 3
+def create_enemy():
+    enemies = ['skeleton', 'zombie', 'alien']
+    if game_level <5: # if block
+        new_enemy = enemies[0]
+    print(new_enemy)
+print(new_enemy)
