993ca0fea62e64e98a3a41d853c964d994d432dd
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-22 11:47:57 +0500

    Updated

---
+++ Python-Problems/36_ModifyGlobal_Variable.py
@@ -0,0 +1,13 @@
+enemies = 1
+
+def increase_enemies():
+    # global enemies # modify global score
+    # enemies +=1
+    
+    # without modifying global score
+    print(f'inside {enemies}')
+    return enemies + 1
+    
+
+increase_enemies()
+print(f'outside: {enemies}')
