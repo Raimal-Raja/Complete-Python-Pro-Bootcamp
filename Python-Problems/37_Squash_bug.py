be8f88a3d35497860410cdb06bdeb2e177740eca
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-01 13:51:11 +0500

    Updated

---
+++ Python-Problems/37_Squash_bug.py
@@ -0,0 +1,7 @@
+pages = 0
+word_per_page =  0
+pages = int(input('Enter number of pages: '))
+word_per_page == int(input('enter number of words: '))
+# print(word_per_page)
+total_word = pages * word_per_page
+print(total_word)
