3f4322120e87502069e7b6bae1b987b3385b7550
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-01 13:52:13 +0500

    Updated

--- Python-Problems/38_bigGunOut.py
+++ Python-Problems/38_bigGunOut.py
@@ -2,7 +2,7 @@ def mutate(a_list):
     b_list = []
     for item in a_list:
         new_item = item * 2
-        
-    b_list.append(new_item)
+        # print(item)
+        b_list.append(new_item)
     print(b_list)
 mutate([1,2,3,4,5])
