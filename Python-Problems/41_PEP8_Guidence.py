10a05421a24a63b3ca7eac6fe66202fe6583be06
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-04 00:31:39 +0500

    Updated

--- Python-Problems/41_PEP8_Guidence.py
+++ Python-Problems/41_PEP8_Guidence.py
@@ -1,10 +1,10 @@
 my_data = {'name': 'Raimal Raja',
            'email': 'thyprofessors@gmail.com'}
 
-
+# print(my_data)
 def myfunction(a, b):
     re = a + b
     return re
 
 
-myfunction(3,5)
+myfunction(3, 5)
