e7d28d3846e730264b117e2e66cafda25ddb7e76
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-05 23:46:16 +0500

    Updated

--- Python_Mini_Projects/Coffee Machine/main.py
+++ Python_Mini_Projects/Coffee Machine/main.py
@@ -1,3 +1,9 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+moneyMachine = MoneyMachine()
+coffeeMaker = CoffeeMaker()
+
+coffeeMaker.report()
+moneyMachine.report()
