c25749a6522abb6b727d8c45c1bcde2401d598f9
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-05 23:55:34 +0500

    Updated

--- Python_Mini_Projects/Coffee Machine/main.py
+++ Python_Mini_Projects/Coffee Machine/main.py
@@ -4,6 +4,19 @@ from money_machine import MoneyMachine
 
 moneyMachine = MoneyMachine()
 coffeeMaker = CoffeeMaker()
+menu = Menu()
+isOn = True
 
 coffeeMaker.report()
 moneyMachine.report()
+
+while isOn:
+    options = menu.get_items()
+    choice = input(f"What would you like? {{options}}:")
+    if choice == "offer":
+        isOn = False
+    elif choice == "report":
+        coffeeMaker.report()
+        moneyMachine.report()
+    else:
+        drink = menu.find_drink()
