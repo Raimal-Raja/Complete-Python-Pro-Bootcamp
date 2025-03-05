9abfbc0ef104f7210e5570a9ebe3007ae8fe3b0f
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-05 23:17:20 +0500

    Updated

--- Python-Problems/43_prettyTable_Module.py
+++ Python-Problems/43_prettyTable_Module.py
@@ -2,4 +2,5 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column('Name', ['Raimal', 'Raja'])
 table.add_column('Language', ['Python', 'Java'])
+table.align = "l"
 print(table)
