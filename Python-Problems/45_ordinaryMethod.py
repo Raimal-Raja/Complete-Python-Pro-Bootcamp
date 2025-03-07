30c4049df905c0aff8d2bbb719410b59ea6015cc
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-07 12:39:25 +0500

    Updated

--- Python-Problems/45_ordinaryMethod.py
+++ Python-Problems/45_ordinaryMethod.py
@@ -13,4 +13,9 @@ class User:
 user_1 = User('049', "Raimal")
 user_2 = User("049", 'Raimal')
 
+# print(user_1.followers)
+user_1.follower(user_2)
 print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
