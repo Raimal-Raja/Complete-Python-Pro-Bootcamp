9c9d7006efadafea54bd00c4b0caa688be8c8a94
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-09 13:47:57 +0500

    Updated

---
+++ Python-Problems/50_importModule.py
@@ -0,0 +1,18 @@
+# Method 1
+# import turtle
+# tim = turtle.Turtle
+
+# Method 2
+from turtle import Turtle
+# tim = Turtle()
+
+# Method 3
+from turtle import *
+# tim = Turtle()
+
+# Method 4 using aliases
+import turtle as t
+# tim = t.Turtle()
+
+import heroes
+print(heroes.gen())
