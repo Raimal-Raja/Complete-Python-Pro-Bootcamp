acfa4e33005fda9ccade1ac4ba9e044926796d88
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-09 12:38:09 +0500

    Updated

---
+++ Python-Problems/48_Turtle_module.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+timmyTheTurtle: Turtle = Turtle()
+timmyTheTurtle.shape('turtle')
+timmyTheTurtle.color('red')
+timmyTheTurtle.forward(100)
+timmyTheTurtle.right(90)
+timmyTheTurtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
