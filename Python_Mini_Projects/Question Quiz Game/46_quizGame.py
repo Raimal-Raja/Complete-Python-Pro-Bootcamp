60b7739cf5b00f0fb76d76e0214c5cbcdbb1586f
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-09 10:37:18 +0500

    Updated

--- Python_Mini_Projects/Question Quiz Game/46_quizGame.py
+++ Python_Mini_Projects/Question Quiz Game/46_quizGame.py
@@ -1,5 +1,6 @@
 from question_model import  Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -8,3 +9,12 @@ for question in question_data:
     new_question = Question(q_text= question_text, q_answer=question_answer)
     question_bank.append(new_question)
 # print(question_bank[0].answer)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_quiz(): # if quiz still has question remaining
+    quiz.next_question()
+
+print("you have completed the quiz")
+print(f"your final score is: {quiz.score}/{quiz.question_number}")
+
