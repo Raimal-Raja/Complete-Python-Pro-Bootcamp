33851bf98b719b0819c15a2465f415a89f1af2ff
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-09 12:50:00 +0500

    Updated

--- Python-Problems/49_drawTurtleSquare.py
+++ Python-Problems/49_drawTurtleSquare.py
@@ -5,12 +5,16 @@ timmy_the_turtle.shape('turtle')
 timmy_the_turtle.color('pink')
 screen: Screen = Screen()
 
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(50)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(50)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 screen.exitonclick()
