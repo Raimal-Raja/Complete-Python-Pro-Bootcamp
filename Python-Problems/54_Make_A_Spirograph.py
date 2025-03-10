9581525731e54bc625b5ce120854d70d990b2613
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-10 09:52:15 +0500

    Updated

---
+++ Python-Problems/54_Make_A_Spirograph.py
@@ -0,0 +1,33 @@
+import turtle as t
+import random
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    b = random.randint(0,255)
+    g = random.randint(0,255)
+    color = (r, b, g)
+    return color
+
+# tim.speed("fast")
+# for _ in range(100):
+#     tim.pensize(2)
+#     tim.color(random_color())
+#     tim.circle(100)
+#     current_heading = tim.heading()
+#     tim.setheading(current_heading +10)
+# tim.circle(100)
+
+tim.speed(30)
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading((tim.heading()+size_of_gap))
+
+draw_spirograph(5)
+
+screen = t.Screen()
+screen.exitonclick()
