8c72165309e2b6f466d87e44084c04875e4d1502
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-14 01:07:32 +0500

    Updated

--- Python-Problems/57_Turtle_Racing_Game.py
+++ Python-Problems/57_Turtle_Racing_Game.py
@@ -6,9 +6,13 @@ screen = Screen()
 # resize window screen
 # screen.setup(500, 400)
 screen.setup(width=500, height=400)
-screen.textinput(title="Make your bet", prompt="which turtle will win the race? Enter a Color: ")
+userBet = screen.textinput(title="Make your bet", prompt="which turtle will win the race? Enter a Color: ")
+colors = ['red', 'orange', 'yellow', 'green', 'purple']
+y_positions = [-70, -48, -18, 20, 50, 80]
 
-
-tim = Turtle()
-
+for turtle_index in range(0, 5):
+    tim = Turtle(shape='turtle')
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-220, y=y_positions[turtle_index])
 screen.exitonclick()
