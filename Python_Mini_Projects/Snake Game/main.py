09b12b12ec25f0ee978cfd9757fc99ec26d5f1ff
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-14 02:26:36 +0500

    Updated

--- Python_Mini_Projects/Snake Game/main.py
+++ Python_Mini_Projects/Snake Game/main.py
@@ -6,19 +6,20 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("My Snake Game")
 
-starting_position = [(0, 0), (-20, 8)]
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
 for position in starting_position:
-    segment_1 = Turtle('square')
-    segment_1.color('white')
+    new_segment = Turtle('square')
+    new_segment.color('white')
+    new_segment.goto(position)
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
-
-segment_3 = Turtle('square')
-segment_3.color("white")
-segment_3.goto(-40, 0)
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+#
+# segment_3 = Turtle('square')
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
 
 screen.exitonclick()
 
