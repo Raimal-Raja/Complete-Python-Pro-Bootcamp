e7cc94abdd048300476e57cabb0e67390d19bb1a
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-14 03:06:50 +0500

    Updated

--- Python_Mini_Projects/Snake Game/main.py
+++ Python_Mini_Projects/Snake Game/main.py
@@ -29,9 +29,17 @@ screen.update()
 
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        screen.update()
-        seg.forward(20)
-        time.sleep(1)
+    screen.update()
+    time.sleep(0.1)
+    # for seg in segments:
+    #     seg.forward(20)
+
+    for seg_num in range(len(segments) -1,0,-1):
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num -1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    # segments[0].left(20)
+    segments[0].forward(20)
+    segments[0].left(90)
 screen.exitonclick()
 
