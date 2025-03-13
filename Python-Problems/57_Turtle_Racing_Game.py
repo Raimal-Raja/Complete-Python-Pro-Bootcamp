2900c0c156fc8d542f09bd34463a977c5ca9aec1
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-14 00:53:34 +0500

    Updated

--- Python-Problems/57_Turtle_Racing_Game.py
+++ Python-Problems/57_Turtle_Racing_Game.py
@@ -3,5 +3,12 @@ from turtle import Turtle, Screen
 timy = Turtle()
 screen = Screen()
 
+# resize window screen
+# screen.setup(500, 400)
+screen.setup(width=500, height=400)
+screen.textinput(title="Make your bet", prompt="which turtle will win the race? Enter a Color: ")
+
+
+tim = Turtle()
 
 screen.exitonclick()
