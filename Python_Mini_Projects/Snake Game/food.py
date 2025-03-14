6147ec18a55cfbafe2ddcba076239a618547c373
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-15 00:45:06 +0500

    Updated

--- Python_Mini_Projects/Snake Game/food.py
+++ Python_Mini_Projects/Snake Game/food.py
@@ -1,2 +1,17 @@
-class Food():
-    pass
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("red")
+        self.penup()
+        self.speed(0)
+        self.refresh()
+
+    def refresh(self):
+        """Moves the food to a random location within the screen bounds."""
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
--- Python_Mini_Projects/Snake Game/main.py
+++ Python_Mini_Projects/Snake Game/main.py
@@ -14,17 +14,28 @@ snake: Snake = Snake()
 food: Food = Food()
 scoreboard: Scoreboard = Scoreboard()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
     if snake.head.distance(food) < 15:
-        food.refesh()
+        food.refresh()
+        snake.extend()
         scoreboard.increase_score()
-    if snake.head.xcor() >280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    for segment in snake.segments:
+        if snake.head.segment(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
