cccb7f8e5da96bb0500995dc4c89436ad47b9be3
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-14 10:38:02 +0500

    Updated

--- Python_Mini_Projects/Snake Game/main.py
+++ Python_Mini_Projects/Snake Game/main.py
@@ -14,7 +14,8 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.refesh()
 
 screen.exitonclick()
