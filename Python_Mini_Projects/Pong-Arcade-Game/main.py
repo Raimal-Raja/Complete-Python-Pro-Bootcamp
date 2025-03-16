127625d334710046430a760e219ffe2d64547334
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-16 10:13:12 +0500

    Updated

--- Python_Mini_Projects/Pong-Arcade-Game/main.py
+++ Python_Mini_Projects/Pong-Arcade-Game/main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreBoard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,11 +10,10 @@ screen.bgcolor('black')
 screen.title('Welcome to the Pong Game')
 screen.tracer(0)
 
-
-r_paddle = Paddle((350,0))
-l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreBoard = ScoreBoard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,19 +21,25 @@ screen.onkey(r_paddle.go_down, 'Down')
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, 's')
 
-
-
-
 game_is_on = True
 while game_is_on:
-    time.sleep(0.2)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
+    # Detect collision of ball with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
-
-    if ball.distance(r_paddle)<50 and ball.xcor()> 320 or ball.distance(l_paddle) < 50 and ball.xcor() < - 320:
+    # Detect collision of paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < - 320:
         ball.bounce_x()
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreBoard.l_point()
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreBoard.r_point()
 
 screen.exitonclick()
