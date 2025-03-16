8c91610cef82af0f71aa43ab434d675137399659
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-16 14:10:15 +0500

    Updated

--- Python_Mini_Projects/Road-Crossing-Game/scoreboard.py
+++ Python_Mini_Projects/Road-Crossing-Game/scoreboard.py
@@ -1,4 +1,24 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-class ScoreBoard:
-    pass
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-50, 260)
+        self.level = 0
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f'Level: {self.level}', align='left',font=FONT)
+    def increase_level(self):
+        self.level += 1
+    def game_over(self):
+        self.goto(0,0)
+        self.write('GAME OVER', align='left',font=FONT)
+
+
