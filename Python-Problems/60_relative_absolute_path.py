4fbbd6951b12b1da3510c5ada73f05adeae68453
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-16 22:54:41 +0500

    Updated

---
+++ Python-Problems/60_relative_absolute_path.py
@@ -0,0 +1,6 @@
+with open("cipher.txt") as file:
+    content = file.read()
+    print(content)
+
+# relative path = complete file path c:// user/foder/file
+# absolute path = working directory path ../python-code/main.py
