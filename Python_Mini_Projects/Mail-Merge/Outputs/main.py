f1fda7dc4630dc8c07ad738416cdfd5aeb3611ec
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-16 23:52:04 +0500

    Updated

--- Python_Mini_Projects/Mail-Merge/Outputs/main.py
+++ Python_Mini_Projects/Mail-Merge/Outputs/main.py
@@ -9,4 +9,6 @@ with open("../Inputs/letter.docs") as letter_file:
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_content.replace(PLACEHOLDER,stripped_name)
-        print(new_letter)
+        # print(new_letter)
+        with open(f"../Outputs/letter_for_{stripped_name}.docs", mode='w') as final_letter:
+            final_letter.write(new_letter)
