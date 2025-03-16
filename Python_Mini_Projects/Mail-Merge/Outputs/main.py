42ee53f8a294da091ffd2d9b126755ab9d7e7864
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-16 23:47:11 +0500

    Updated

---
+++ Python_Mini_Projects/Mail-Merge/Outputs/main.py
@@ -0,0 +1,12 @@
+PLACEHOLDER = "[name]"
+
+with open('../Inputs/names.txt') as names_file:
+    names = names_file.readlines()
+    print(names)
+
+with open("../Inputs/letter.docs") as letter_file:
+    letter_content = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER,stripped_name)
+        print(new_letter)
