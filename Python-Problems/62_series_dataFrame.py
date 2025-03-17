4e075c757bc7d0d665b8e0116c381b1b9797d78e
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-17 14:43:34 +0500

    Updated

--- Python-Problems/62_series_dataFrame.py
+++ Python-Problems/62_series_dataFrame.py
@@ -1,14 +1,19 @@
 import pandas as pd
+
 data = pd.read_csv('weather_data.csv')
 
+# convert to the list
 temp_list = data['temp'].to_list()
 print(temp_list)
 
-average = sum(temp_list)/len(temp_list)
+# find the average using sum function
+average = sum(temp_list) / len(temp_list)
 print(average)
 
+# find mean
 mean = data['temp'].mean()
 print(mean)
 
+# find mode
 mode = data['temp'].mode()
 print(mode)
