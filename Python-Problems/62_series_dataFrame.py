7599df4917f4892b8de895e0fa5d805c1ebedf51
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-17 14:52:20 +0500

    Updated

--- Python-Problems/62_series_dataFrame.py
+++ Python-Problems/62_series_dataFrame.py
@@ -1,19 +1,30 @@
 import pandas as pd
 
 data = pd.read_csv('weather_data.csv')
+#
+# # convert to the list
+# temp_list = data['temp'].to_list()
+# print(temp_list)
+#
+# # find the average using sum function
+# average = sum(temp_list) / len(temp_list)
+# print(average)
+#
+# # find mean
+# mean = data['temp'].mean()
+# print(mean)
+#
+# # find mode
+# mode = data['temp'].mode()
+# print(mode)
 
-# convert to the list
-temp_list = data['temp'].to_list()
-print(temp_list)
+# find max
+# max_temp = data['temp'].max()
+# print(max_temp)
 
-# find the average using sum function
-average = sum(temp_list) / len(temp_list)
-print(average)
+# data_condition = data['condition']
+# print(data_condition)
 
-# find mean
-mean = data['temp'].mean()
-print(mean)
-
-# find mode
-mode = data['temp'].mode()
-print(mode)
+# get data columns directly
+col = data.condition
+print(col)
