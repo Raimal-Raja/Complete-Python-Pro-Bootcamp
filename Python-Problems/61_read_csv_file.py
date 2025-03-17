8aa781b50bbd2931796b3c154efb169a5e853503
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-17 13:57:43 +0500

    Updated

--- Python-Problems/61_read_csv_file.py
+++ Python-Problems/61_read_csv_file.py
@@ -1,14 +1,17 @@
 # with open("weather_data.csv") as file:
 #     content = file.readlines()
 #     print(content)
+#
+# import csv
 
-import csv
-from typing import List
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        if row[1] != "temp":
-            temperature.append(int(row[1]))
-    print(temperature)
+import pandas
+data = pandas.read_csv("weather_data.csv")
+print(data['temp'])
