341c2ec690bbc989df651926eb06ed8f58abd3e5
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-05 00:10:47 +0500

    Updated

--- Python_Mini_Projects/US-State-Quiz-Game/main.py
+++ Python_Mini_Projects/US-State-Quiz-Game/main.py
@@ -10,17 +10,25 @@ data = pd.read_csv("50_states.csv")
 all_states_list = data.state.to_list()
 
 print(all_states_list)
-answer = screen.textinput(title="Guess the State", prompt="What's the next state's name")
 # print(answer)
 
-if answer in all_states_list:
-    timy = turtle.Turtle()
-    timy.hideturtle()
-    timy.penup()
-    row = data[data.state == answer]
-    timy.goto(int(row.x),int(row.y))
-    # timy.write(answer)
+guess_list = []
+n = 50
+while len(guess_list) < n:
+    answer = screen.textinput(title=f"{len(guess_list)}/50 Correct States", prompt="What's the next state's name").title()
+    if answer in all_states_list:
+        guess_list.append(answer)
+        timy = turtle.Turtle()
+        timy.hideturtle()
+        timy.penup()
+        row = data[data.state == answer]
+        timy.goto(int(row.x),int(row.y))
+        # timy.write(answer)
 
-    #.item() method will extract first element
-    timy.write(row.state.item())
+        #.item() method will extract first element
+        timy.write(row.state.item())
+    elif answer =='stop':
+        n =0
+
+
 screen.exitonclick()
