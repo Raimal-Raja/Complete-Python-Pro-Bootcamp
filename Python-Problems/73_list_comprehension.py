ec17a3452ab0a49527fc932086744918bef2c3af
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-13 02:31:17 +0500

    updated

---
+++ Python-Problems/73_list_comprehension.py
@@ -0,0 +1,8 @@
+# mylist = [1,2,3,4,4]
+#
+# new_list = [item *2 for item in mylist]
+#
+# print(new_list)
+
+my_list = [item *2 for item in range(1,6)]
+print(my_list)
---
+++ Python-Problems/74_conditional_list_comprehension.py
@@ -0,0 +1,2 @@
+odd_num_list = [num for num in range(1,11) if num%2 != 0]
+print(odd_num_list)
