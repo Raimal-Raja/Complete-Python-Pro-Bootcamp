aea96e7b030df2e7dc785bf312ea98f52139e6de
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-19 18:29:26 +0500

    updated

---
+++ Python-Problems/82_iterate_dict_over_pandas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# student = {"students":['Raimal Raja', "Haroon", "Talha"],
+#            'Score':[53,76,84,]}
+# new_student = pd.DataFrame(student)
+# new_student.to_csv("student.csv")
+
+
+file = pd.read_csv('student.csv')
+for(index, row) in file.iterrows():
+    print(row)
