5e1eaf03ad28e74af864ea72647677e7f8dc1b80
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-27 02:26:44 +0500

    updated

--- Python_Mini_Projects/GUI_password_manager/main.py
+++ Python_Mini_Projects/GUI_password_manager/main.py
@@ -2,15 +2,27 @@ from tkinter import *
 
 
 
+def getValue():
+    email = username.get()
+    password_ = password.get()
+    website_ = website.get()
+    with open("password.txt",mode='w') as file:
+        file.write(f"Website: {website_} Email: {email} Password: {password_}" )
+    return list
+
 # initializing window
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Entry
 website = Entry(width=35)
+website.focus()
 username = Entry(width=35)
+username.focus()
+username.insert(0,"eg; professor@gmail.com")
 password = Entry(width=35)
+password.focus()
 
 # Labels
 website_label = Label(text="Website")
@@ -18,18 +30,20 @@ username_label = Label(text="Email/Username")
 Password_label = Label(text="Password")
 
 # Buttons
-add = Button(text="Add")
+add = Button(text="Add", command=getValue)
+# getValue()
+
 generate_password = Button(text="Generate Password")
 
 
 # background window
 canvas = Canvas(height=200, width=200)
 # logo_img = PhotoImage(file="logo.png")
-# canvas.create_image(100, image=logo_img)
+# canvas.create_image(100, 100,image=logo_img)
 #
 #
 # # Packing
-canvas.grid()
+canvas.grid(row=0, column=1)
 # cells
 website.grid(column=1, row=1,columnspan=2)
 username.grid(column=1, row=2, columnspan=2)
@@ -43,6 +57,8 @@ Password_label.grid(column=0, row=3)
 # Button
 generate_password.grid(column=3, row=3)
 add.grid(column=1, row=4)
+
+
 
 
 window.mainloop()
