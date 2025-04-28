accc9c3a64cd0a9f53a3bd704cbf746449fcad46
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-29 02:44:05 +0500

    updated

--- Python_Mini_Projects/GUI_password_manager/main.py
+++ Python_Mini_Projects/GUI_password_manager/main.py
@@ -1,6 +1,38 @@
 from tkinter import *
 from tkinter.messagebox import showinfo, askokcancel
+from random import choice, randint, shuffle
 
+###################### Password Generator
+
+
+def passwordGenerator():
+    letters = [
+        'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+        'z', 'A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+        'X', 'Z'
+    ]
+    symbols = [
+        '!', '@', '#', '$', '%', '^', '&', '*', '_', '\\', '|', '?', '`', '~'
+    ]
+
+    numbers = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    ]
+
+
+    password_letters = [choice(letters) for _ in range(randint(8,10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters+password_symbols+password_numbers
+    shuffle(password_list)
+
+    generated_password  = "".join(password_list)
+    password.insert(0, generated_password)
+
+
+
+####################### get values from cells
 
 def getValue():
     email = username.get()
@@ -20,12 +52,12 @@ def getValue():
                 website.delete(0,END)
 
 
-# initializing window
+############### initializing window
 window = Tk()
 window.title("Password Manager")
 window.config(padx=10, pady=10)
 
-# Entry
+############## Entry
 website = Entry(width=35)
 website.focus()
 username = Entry(width=35)
@@ -34,37 +66,35 @@ username.insert(0,"eg; professor@gmail.com")
 password = Entry(width=35)
 password.focus()
 
-# Labels
+############## Labels
 website_label = Label(text="Website")
 username_label = Label(text="Email/Username")
 Password_label = Label(text="Password")
 
-# Buttons
+############## Buttons
 add = Button(text="Add", command=getValue)
-# getValue()
-
-generate_password = Button(text="Generate Password")
+generate_password = Button(text="Generate Password", command=passwordGenerator)
 
 
-# background window
+############# background window
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100,image=logo_img)
 
 #
-# # Packing
+############### Packing
 canvas.grid(row=0, column=1)
-# cells
+############### cells
 website.grid(column=1, row=1,columnspan=2)
 username.grid(column=1, row=2, columnspan=2)
 password.grid(column=1, row=3)
 
-# Label
+############### Label
 website_label.grid(column=0, row=1)
 username_label.grid(column=0, row=2)
 Password_label.grid(column=0, row=3)
 
-# Button
+################## Button
 generate_password.grid(column=3, row=3)
 add.grid(column=1, row=4)
 
